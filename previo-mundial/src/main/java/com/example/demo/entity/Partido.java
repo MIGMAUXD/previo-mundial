913a9a82ec2f4eb005c2a1808211fedc29cabda8
MIGMAUXD <jonathanmauriciovabo@ufps.edu.co>
2023-05-29 10:44:04 -0500

    Commit punto 1 completado

---
+++ previo-mundial/src/main/java/com/example/demo/entity/Partido.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Data;
+
+@Data
+@Entity
+public class Partido implements Serializable{
+	@Id
+	@SequenceGenerator(name="partido_id_seq",allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "partido_id_seq")
+	private Integer id;
+	private Date fecha;
+	@ManyToOne
+	@JoinColumn(name="estadio_id")
+	private Estadio estadio;
+	@JsonIgnore
+	@OneToMany(mappedBy = "partido")
+	private List<Resultado>resultados;
+}
